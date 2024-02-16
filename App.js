262550bfbd7e3dd595d76391ae5064095b2d3c2d
Rufus2018 <sekou950761@gmail.com>
2024-02-15 19:58:52 -0500

    On Using ImagePicker

---
+++ App.js
@@ -0,0 +1,52 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import Button from './components/Button'; 
+import ImageViewer from './components/ImageViewer';
+import * as ImagePicker from 'expo-image-picker'; 
+const PlaceholderImage = require('./assets/images/background-image.png'); 
+
+export default function App() {
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      console.log(result);
+    } else {
+      alert('You did not select any image.');
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+      <ImageViewer placeholderImageSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Choose a photo" />
+        <Button label="Use this photo" />
+      </View>
+      <StatusBar style="auto" /> 
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18, 
+  },  
+});
